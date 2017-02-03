bccfdf04bfaede45546d917f7d57d81aade78043
Pavlo Bystritsky <p.bystritsky@yandex.ru>
2017-02-03 13:36:21 +0200

    db_init fix

--- db_init.sh
+++ db_init.sh
@@ -1,1 +1,4 @@
 mysql -uroot -e "create database stepic_web;"
+mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY '1234';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
