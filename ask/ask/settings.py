bd3e381ec61513ed943a3819b0f3619ec6237d1c
Pavel <p.bystritsky@yandex.ru>
2017-02-11 00:48:55 +0300

    Added SETTINGS template

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -65,6 +65,17 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 #from local_settings import *
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'stepic_web',
+#        'USER': 'root',
+#        'PASSWORD': '',
+#        'HOST': '127.0.0.1',
+#        'PORT': '3306',
+#    }
+#}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
