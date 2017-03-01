24b027cda568aa944aa95a04a174ff12125cc177
Pavel <p.bystritsky@yandex.ru>
2017-03-01 20:43:33 +0300

    Some improvements for simpler deployment

--- db_init.sh
+++ db_init.sh
@@ -1,6 +1,6 @@
 sudo /etc/init.d/mysql start﻿
 mysql -uroot -e "create database stepic_web;"
-mv ask/ask/default_settings.py local_settings.py
-mv ask/qa/default_settings.py local_settings.py
+cp ask/ask/default_settings.py ask/ask/local_settings.py
+cp ask/qa/default_settings.py ask/qa/local_settings.py
 ask/manage.py syncdb
 ask/manage.py runserver 0.0.0.0:80 &
