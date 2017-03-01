33434ea1a965f75bd5b71bfe7041442c85c26a71
Pavel <p.bystritsky@yandex.ru>
2017-03-01 21:41:58 +0300

    Some adaptations to Django 1.10.5

--- db_init.sh
+++ db_init.sh
@@ -2,5 +2,7 @@ sudo /etc/init.d/mysql start﻿
 mysql -uroot -e "create database stepic_web;"
 cp ask/ask/default_settings.py ask/ask/local_settings.py
 cp ask/qa/default_settings.py ask/qa/local_settings.py
-ask/manage.py syncdb
+ask/manage.py makemigrations ask
+ask/manage.py makemigrations qa
+ask/manage.py migrate
 ask/manage.py runserver 0.0.0.0:80 &
